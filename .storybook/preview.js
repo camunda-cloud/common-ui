97cbdaa2572a75b4540ef7057b9e00ef053b83c1
Alexander Trefz <alexander.trefz@camunda.com>
2020-11-30 13:34:46 +0100

    Improve Documentation Structure
    Add some common-sense folders, sort folders on top.
    Minor text changes for cm-checkbox & cm-toggle.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import { addParameters } from '@storybook/react';
+import { addParameters } from '@storybook/react'
 
 addParameters({
 	viewMode: 'docs',
@@ -6,15 +6,25 @@ addParameters({
 		storySort: (a, b) => {
 			// TODO: Define a full ordering list and set up a generic handling of such a list
 
-			if (a[1].kind === "Introduction") {
+			// NOTE: Put the Introduction on the very top, afterwards folders on top, in alphabetical order.
+
+			if (a[1].kind === 'Introduction') {
 				return -1
 			}
 
-			if (b[1].kind === "Introduction") {
+			if (b[1].kind === 'Introduction') {
 				return 1
 			}
 
-			return a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, { numeric: true })
-		}
+			const folderDepthDelta =
+				b[1].kind.split('/').length - a[1].kind.split('/').length
+			if (folderDepthDelta !== 0) {
+				return folderDepthDelta
+			}
+
+			return a[1].kind === b[1].kind
+				? 0
+				: a[1].id.localeCompare(b[1].id, { numeric: true })
+		},
 	},
-});
+})
