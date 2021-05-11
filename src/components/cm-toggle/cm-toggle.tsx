8fdd3e9cc0393a2b4255296fb97cc3390196c113
Alexander Trefz <alexander.trefz@camunda.com>
2021-05-11 13:18:54 +0200

    Refactor cm-toggle to utilise :focus-visible

--- src/components/cm-toggle/cm-toggle.tsx
+++ src/components/cm-toggle/cm-toggle.tsx
@@ -6,7 +6,6 @@ import {
 	Listen,
 	Event,
 	EventEmitter,
-	State,
 	Method,
 } from '@stencil/core'
 
@@ -22,32 +21,15 @@ export class CmToggle {
 
 	@Event() cmInput: EventEmitter<{ isChecked: boolean }>
 
-	@State() latestFocusWasClick: boolean = false
 	toggle: HTMLDivElement
 
 	@Listen('keydown')
 	handleKeyDown(event: KeyboardEvent) {
-		if (
-			!(event.key === 'Control') &&
-			!(event.key === 'Meta') &&
-			!(event.key === 'Alt') &&
-			!(event.key === 'Shift' && event.code !== 'Tab')
-		) {
-			this.latestFocusWasClick = false
-		}
-
 		if (event.key === ' ') {
 			if (!this.disabled) {
 				this.toggleCheck()
 			}
 		}
-	}
-
-	// Prevent clicks from giving visual focus
-	@Listen('mousedown', { passive: false })
-	handleMouseDown(event: MouseEvent) {
-		this.latestFocusWasClick = true
-		event.preventDefault()
 	}
 
 	@Listen('click')
@@ -75,17 +57,10 @@ export class CmToggle {
 		this.cmInput.emit({ isChecked: this.checked })
 	}
 
-	componentWillUpdate() {
-		if (this.latestFocusWasClick) {
-			this.toggle.focus()
-		}
-	}
-
 	render() {
 		let toggleClasses = {
 			toggle: true,
 			checked: this.checked,
-			clicked: this.latestFocusWasClick,
 			disabled: this.disabled,
 		}
 
