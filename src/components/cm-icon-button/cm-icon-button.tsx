5b954f4cbd3169e1bb18b39348e24a7222f35e20
Alexander Trefz <alexander.trefz@camunda.com>
2021-05-14 10:54:16 +0200

    Improve @Method() for cm-icon-button to respect disabled and only fire events when appropriate

--- src/components/cm-icon-button/cm-icon-button.tsx
+++ src/components/cm-icon-button/cm-icon-button.tsx
@@ -43,11 +43,13 @@ export class CmIconButton implements ComponentInterface {
 	@Element() el: HTMLElement
 
 	/**
-	 * Triggers a virtual press.
+	 * Triggers the press event. Respects the disabled state unless forced.
 	 */
 	@Method()
-	async press() {
-		this.cmPress.emit()
+	async press(options: { forcePress?: boolean } = {}) {
+		if (!this.disabled || options.forcePress) {
+			this.cmPress.emit()
+		}
 	}
 
 	@Listen('click')
