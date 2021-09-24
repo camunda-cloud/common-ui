2d0069596f816129d896f74860b5dc459ff21f09
Hüsna Uygur <husna.uygur@camunda.com>
2021-09-24 12:51:00 +0200

    Increase tests for cm-button

--- src/components/cm-button/test/cm-button.e2e.ts
+++ src/components/cm-button/test/cm-button.e2e.ts
@@ -8,4 +8,88 @@ describe('cm-button', () => {
 		const element = await page.find('cm-button')
 		expect(element).toHaveClass('hydrated')
 	})
+
+	it('calls cmPress event on button click', async () => {
+		const page = await newE2EPage()
+
+		await page.setContent('<cm-button></cm-button>')
+		const spyCmPress = await page.spyOnEvent('cmPress')
+
+		const button = await page.find('cm-button >>> [role="button"]')
+
+		button.click()
+		await page.waitForChanges()
+
+		expect(spyCmPress).toHaveReceivedEventTimes(1)
+	})
+
+	it('calls cmPress event on keyboard event "Enter"', async () => {
+		const page = await newE2EPage()
+
+		await page.setContent('<cm-button></cm-button>')
+		const spyCmPress = await page.spyOnEvent('cmPress')
+
+		await page.keyboard.down('Tab')
+		await page.keyboard.down('Enter')
+		await page.waitForChanges()
+
+		expect(spyCmPress).toHaveReceivedEventTimes(1)
+	})
+
+	it('calls cmPress event on keyboard event "Space"', async () => {
+		const page = await newE2EPage()
+
+		await page.setContent('<cm-button></cm-button>')
+		const spyCmPress = await page.spyOnEvent('cmPress')
+
+		await page.keyboard.down('Tab')
+		await page.keyboard.down(' ')
+		await page.waitForChanges()
+
+		expect(spyCmPress).toHaveReceivedEventTimes(1)
+	})
+
+	it('does not call cmPress event on disabled button click', async () => {
+		const page = await newE2EPage()
+
+		await page.setContent('<cm-button disabled></cm-button>')
+		const spyCmPress = await page.spyOnEvent('cmPress')
+
+		const button = await page.find('cm-button >>> [role="button"]')
+
+		button.click()
+		await page.waitForChanges()
+
+		expect(spyCmPress).not.toHaveReceivedEvent()
+	})
+
+	it('does not call cmPress event on loading button click', async () => {
+		const page = await newE2EPage()
+
+		await page.setContent('<cm-button loading></cm-button>')
+		const spyCmPress = await page.spyOnEvent('cmPress')
+
+		const button = await page.find('cm-button >>> [role="button"]')
+
+		button.click()
+		await page.waitForChanges()
+
+		expect(spyCmPress).not.toHaveReceivedEvent()
+	})
+
+	it('calls cmPress event on loading link button click', async () => {
+		const page = await newE2EPage()
+
+		await page.setContent(
+			'<cm-button loading appearance="link" label="Test"></cm-button>',
+		)
+		const spyCmPress = await page.spyOnEvent('cmPress')
+
+		const button = await page.find('cm-button >>> [role="button"]')
+
+		button.click()
+		await page.waitForChanges()
+
+		expect(spyCmPress).toHaveReceivedEventTimes(1)
+	})
 })
--- src/components/cm-button/test/cm-button.spec.tsx
+++ src/components/cm-button/test/cm-button.spec.tsx
@@ -1,18 +1,158 @@
+import { h } from '@stencil/core'
 import { newSpecPage } from '@stencil/core/testing'
 import { CmButton } from '../cm-button'
 
 describe('cm-button', () => {
-	it('renders', async () => {
+	it('renders primary button', async () => {
 		const page = await newSpecPage({
 			components: [CmButton],
-			html: `<cm-button></cm-button>`,
+			template: () => (
+				<cm-button appearance="primary" label="Test"></cm-button>
+			),
+		})
+		expect(page.root).toEqualHtml(`
+			<cm-button>
+				<mock:shadow-root>
+					<div class="Light initialRender primary normal" role="button" tabindex="0">
+						<cm-loader color="light" size="small"></cm-loader>
+						<span>Test</span>
+					</div>
+				</mock:shadow-root>
+			</cm-button>
+		`)
+	})
+
+	it('renders secondary button', async () => {
+		const page = await newSpecPage({
+			components: [CmButton],
+			template: () => (
+				<cm-button appearance="secondary" label="Test"></cm-button>
+			),
+		})
+		expect(page.root).toEqualHtml(`
+			<cm-button>
+				<mock:shadow-root>
+					<div class="Light initialRender secondary normal" role="button" tabindex="0">
+						<cm-loader color="dark" size="small"></cm-loader>
+						<span>Test</span>
+					</div>
+				</mock:shadow-root>
+			</cm-button>
+		`)
+	})
+
+	it('renders link button', async () => {
+		const page = await newSpecPage({
+			components: [CmButton],
+			template: () => (
+				<cm-button appearance="link" label="Test"></cm-button>
+			),
+		})
+		expect(page.root).toEqualHtml(`
+			<cm-button>
+				<mock:shadow-root>
+					<div class="Light initialRender link normal" role="button" tabindex="0">
+						<cm-loader color="dark" size="small"></cm-loader>
+						<span>Test</span>
+					</div>
+				</mock:shadow-root>
+			</cm-button>
+		`)
+	})
+
+	it('renders main button', async () => {
+		const page = await newSpecPage({
+			components: [CmButton],
+			template: () => (
+				<cm-button appearance="main" label="Test"></cm-button>
+			),
 		})
 		expect(page.root).toEqualHtml(`
 			<cm-button>
 				<mock:shadow-root>
 					<div class="Light initialRender main normal" role="button" tabindex="0">
 						<cm-loader color="dark" size="small"></cm-loader>
-						<span></span>
+						<span>Test</span>
+					</div>
+				</mock:shadow-root>
+			</cm-button>
+		`)
+	})
+
+	it('renders danger button', async () => {
+		const page = await newSpecPage({
+			components: [CmButton],
+			template: () => (
+				<cm-button appearance="danger" label="Test"></cm-button>
+			),
+		})
+		expect(page.root).toEqualHtml(`
+			<cm-button>
+				<mock:shadow-root>
+					<div class="Light initialRender danger normal" role="button" tabindex="0">
+						<cm-loader color="light" size="small"></cm-loader>
+						<span>Test</span>
+					</div>
+				</mock:shadow-root>
+			</cm-button>
+		`)
+	})
+
+	it('renders disabled button', async () => {
+		const page = await newSpecPage({
+			components: [CmButton],
+			template: () => (
+				<cm-button appearance="main" disabled label="Test"></cm-button>
+			),
+		})
+		expect(page.root).toEqualHtml(`
+			<cm-button>
+				<mock:shadow-root>
+					<div aria-disabled="" class="Light disabled initialRender main normal" role="button" tabindex="-1">
+						<cm-loader color="dark" size="small"></cm-loader>
+						<span>Test</span>
+					</div>
+				</mock:shadow-root>
+			</cm-button>
+		`)
+	})
+
+	it('renders small button', async () => {
+		const page = await newSpecPage({
+			components: [CmButton],
+			template: () => (
+				<cm-button
+					appearance="main"
+					label="Test"
+					size="small"
+				></cm-button>
+			),
+		})
+		expect(page.root).toEqualHtml(`
+			<cm-button>
+				<mock:shadow-root>
+					<div class="Light initialRender main small" role="button" tabindex="0">
+						<cm-loader color="dark" size="small"></cm-loader>
+						<span>Test</span>
+					</div>
+				</mock:shadow-root>
+			</cm-button>
+		`)
+	})
+
+	it('renders loading button', async () => {
+		const page = await newSpecPage({
+			components: [CmButton],
+			template: () => (
+				<cm-button appearance="main" label="Test" loading></cm-button>
+			),
+		})
+		expect(page.root).toEqualHtml(`
+			<cm-button>
+				<mock:shadow-root>
+					<div class="Light initialRender loading main normal" role="button" tabindex="0">
+						<cm-loader color="dark" size="small"></cm-loader>
+						<span>Test</span>
 					</div>
 				</mock:shadow-root>
 			</cm-button>
