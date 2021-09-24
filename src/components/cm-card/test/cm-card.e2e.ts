4e32062364024c708985f45472516ad88bb6e526
Hüsna Uygur <husna.uygur@camunda.com>
2021-09-24 10:42:36 +0200

    Increase tests for cm-card

--- src/components/cm-card/test/cm-card.e2e.ts
+++ src/components/cm-card/test/cm-card.e2e.ts
@@ -8,4 +8,18 @@ describe('cm-card', () => {
 		const element = await page.find('cm-card')
 		expect(element).toHaveClass('hydrated')
 	})
+
+	it('calls cmDismissed event on dismiss button click', async () => {
+		const page = await newE2EPage()
+
+		await page.setContent('<cm-card is-dismissable="true"></cm-card>')
+		const spyCmDismissed = await page.spyOnEvent('cmDismissed')
+
+		const closeButton = await page.find('cm-card >>> #dismissButton')
+
+		closeButton.click()
+		await page.waitForChanges()
+
+		expect(spyCmDismissed).toHaveReceivedEvent()
+	})
 })
--- src/components/cm-card/test/cm-card.spec.tsx
+++ src/components/cm-card/test/cm-card.spec.tsx
@@ -1,5 +1,6 @@
 import { newSpecPage } from '@stencil/core/testing'
 import { CmCard } from '../cm-card'
+import { h } from '@stencil/core'
 
 describe('cm-card', () => {
 	it('renders', async () => {
@@ -16,6 +17,27 @@ describe('cm-card', () => {
 							<slot></slot>
 						</div>
 						<slot name="footer"></slot>
+					</div>
+				</mock:shadow-root>
+			</cm-card>
+		`)
+	})
+
+	it('has dismiss button', async () => {
+		const page = await newSpecPage({
+			components: [CmCard],
+			template: () => <cm-card isDismissable={true}></cm-card>,
+		})
+		expect(page.root).toEqualHtml(`
+			<cm-card>
+				<mock:shadow-root>
+					<div class="container">
+						<slot name="header"></slot>
+						<div class="content">
+							<slot></slot>
+						</div>
+						<slot name="footer"></slot>
+						<cm-icon-button icon="closeLarge" id="dismissButton"></cm-icon-button>
 					</div>
 				</mock:shadow-root>
 			</cm-card>
