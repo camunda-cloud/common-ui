66b992e3f6c23a1c16a703de039d553716770aad
Hüsna Uygur <husna.uygur@camunda.com>
2021-10-12 15:00:25 +0300

    Increase tests for cm-checkbox-group

--- src/components/cm-checkbox-group/test/cm-checkbox-group.spec.tsx
+++ src/components/cm-checkbox-group/test/cm-checkbox-group.spec.tsx
@@ -7,6 +7,7 @@ describe('cm-checkbox-group', () => {
 			components: [CmCheckboxGroup],
 			html: `<cm-checkbox-group></cm-checkbox-group>`,
 		})
+
 		expect(page.root).toEqualHtml(`
 			<cm-checkbox-group>
 				<mock:shadow-root>
@@ -14,6 +15,37 @@ describe('cm-checkbox-group', () => {
 						<slot></slot>
 					</div>
 				</mock:shadow-root>
+			</cm-checkbox-group>
+		`)
+	})
+
+	it('renders a group of checkboxes with a label', async () => {
+		const page = await newSpecPage({
+			components: [CmCheckboxGroup],
+			html: `<cm-checkbox-group label="Checkboxes">
+			<cm-checkbox
+				label="Checkbox"
+				helper-text="Helper Text"
+			></cm-checkbox>
+
+			<cm-checkbox
+				form-name="requiredCheckbox"
+				label="Required Checkbox"
+				required
+			></cm-checkbox>
+		</cm-checkbox-group>`,
+		})
+
+		expect(page.root).toEqualHtml(`
+			<cm-checkbox-group label="Checkboxes">
+				<mock:shadow-root>
+					<cm-text appearance="emphasis" id="label">Checkboxes</cm-text>
+					<div class="container">
+						<slot></slot>
+					</div>
+				</mock:shadow-root>
+				<cm-checkbox helper-text="Helper Text" label="Checkbox"></cm-checkbox>
+				<cm-checkbox form-name="requiredCheckbox" label="Required Checkbox" required=""></cm-checkbox>
 			</cm-checkbox-group>
 		`)
 	})
